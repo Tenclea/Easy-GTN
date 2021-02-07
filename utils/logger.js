1a8b6f6c60e1bea20c034c96e7c483c7e67bc7a6
Tenclea <tencleaynot@gmail.com>
2021-02-07 22:37:47 +0100

    Update logger

--- utils/logger.js
+++ utils/logger.js
@@ -7,12 +7,18 @@ const debugMode = JSON.parse(readFileSync('./config.json')).debugMode;
 module.exports = winston.createLogger({
 	transports: [new winston.transports.Console()],
 	format: winston.format.printf(log => {
+		const date = new Date();
+		let times = [date.getHours(), date.getMinutes(), date.getSeconds()];
+		times = times.map(t => { if (t < 10) { t = '0' + t; } return t; });
+
+		const time = chalk.magenta(times.join(':')) + ' ';
 		const message = ` » ${log.message}`;
-		if (log.level === 'info') return chalk.greenBright(`[${log.level.toUpperCase()}] `) + message;
-		else if (log.level === 'warn') return chalk.yellow(`[${log.level.toUpperCase()}] `) + message;
-		else if (log.level === 'error') return chalk.red(`[${log.level.toUpperCase()}]`) + message;
-		else if (log.level === 'debug') return chalk.blue(`[${log.level.toUpperCase()}]`) + message;
-		else return `[${log.level.toUpperCase()}]` + message;
+
+		if (log.level === 'info') return time + chalk.greenBright(`[${log.level.toUpperCase()}] `) + message;
+		else if (log.level === 'warn') return time + chalk.yellow(`[${log.level.toUpperCase()}] `) + message;
+		else if (log.level === 'error') return time + chalk.red(`[${log.level.toUpperCase()}]`) + message;
+		else if (log.level === 'debug') return time + chalk.blue(`[${log.level.toUpperCase()}]`) + message;
+		else return time + `[${log.level.toUpperCase()}]` + message;
 	}),
 	level: debugMode ? 'debug' : 'info',
 });
